75621e9691163ff1d4df7dc6b221e79ab538333c
yopaaa <pusi.ng027@gmail.com>
2026-06-26 20:46:22 +0700

    document

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index', [
+        "title" => "Bank Sampah Digital",
+    ]);
 });
+
+Route::view('/login', 'auth.login')->name('login');
+Route::view('/register', 'auth.register')->name('register');
+Route::view('/profile', 'profile')->middleware('auth')->name('profile');
+Route::get('/logout', function () {
+    Auth::logout();
+    return redirect()->route('login');
+})->name('logout');
